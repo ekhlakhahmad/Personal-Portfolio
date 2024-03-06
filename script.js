06aa7fbbaa3b0f61cacf109277ce613abd03f726
Ekhlakh Ahmad <ekhlakhahmad95@gmail.com>
2024-03-07 01:47:55 +0530

    create more atractive and add animation

--- script.js
+++ script.js
@@ -12,3 +12,74 @@ window.addEventListener("resize", () => {
     hamburgerMenu.classList.remove("active");
   }
 });
+
+
+// GSAP Animation
+
+gsap.registerPlugin(ScrollTrigger);
+
+var tl = gsap.timeline();
+tl.from(".header-left-side", {
+  opacity: 0,
+  y: -60,
+  duration: 0.5,
+  delay: 0.5,
+  stagger: 0.1,
+});
+tl.from(".navbar-menu li", {
+  opacity: 0,
+  y: 60,
+  duration: 0.5,
+  delay: 0.5,
+  stagger: 0.1,
+});
+gsap.from(".home-left-side h3,h1,h2", {
+  x: -700,
+  duration: 0.5,
+  delay: 0.5,
+  stagger: 0.1,
+});
+gsap.from(".home-right-side", {
+  x: 500,
+  duration: 0.5,
+  delay: 0.5,
+  stagger: 0.1,
+});
+gsap.from(".about-left-side", {
+  opacity: 0,
+  x: -200,
+  // rotateZ: 360s,
+  scrollTrigger: {
+    trigger: ".about-left-side",
+    scrub: true,
+    start: "top center",
+    end: "top 20%",
+  },
+});
+const skillRightSide = document.querySelector('.skill-right-side');
+
+ScrollTrigger.create({
+    trigger: skillRightSide,
+    start: 'top 80%',
+    end: 'bottom 20%',
+    onEnter: () => {
+        const bars = document.querySelectorAll('.bar');
+        bars.forEach(bar => {
+            const width = bar.getAttribute('data-width');
+          gsap.to(bar, {
+                width: `${width}%`,
+                backgroundColor: "#1e3a8a", 
+                duration: 0.6
+            });
+        });
+    },
+    onLeaveBack: () => {
+        const bars = document.querySelectorAll('.bar');
+        bars.forEach(bar => {
+            gsap.to(bar, {
+                width: '20%',
+                duration: 0.1 
+            });
+        });
+    }
+});
